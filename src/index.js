c7dfbeef0a19cc781cffc356dd09a8ee3ac25f29
nikkuts <nickkuts888@gmail.com>
2024-04-20 21:11:26 +0300

    update react

--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
 import { StrictMode } from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react';
 import {BrowserRouter} from 'react-router-dom';
 import { store, persistor } from "./redux/store";
 import App from './components/App';
-// import {BASE_NAME} from './constants';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <StrictMode>
+ReactDOM.render(
+  <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter 
-          // basename={BASE_NAME}
-        >
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </PersistGate>
     </Provider>
-  </StrictMode>
+  </StrictMode>,
+  document.getElementById('root')
 );
+
+// import { StrictMode } from "react";
+// import ReactDOM from 'react-dom';
+// import { Provider } from "react-redux";
+// import { PersistGate } from 'redux-persist/integration/react';
+// import {BrowserRouter} from 'react-router-dom';
+// import { store, persistor } from "./redux/store";
+// import App from './components/App';
+// // import {BASE_NAME} from './constants';
+// import './index.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <StrictMode>
+//     <Provider store={store}>
+//       <PersistGate loading={null} persistor={persistor}>
+//         <BrowserRouter 
+//           // basename={BASE_NAME}
+//         >
+//           <App />
+//         </BrowserRouter>
+//       </PersistGate>
+//     </Provider>
+//   </StrictMode>
+// );
